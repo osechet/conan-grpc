10260e3302373d613fdc66205cad33b1cdd5242c
Olivier Sechet <osechet@gmail.com>
2019-08-27 15:16:05 +0200

    Force building missing dependencies

--- build.py
+++ build.py
@@ -4,7 +4,7 @@ from conan.packager import ConanMultiPackager
 def main():
     """ Main function """
 
-    builder = ConanMultiPackager()
+    builder = ConanMultiPackager(build_policy="missing")
     builder.add_common_builds(pure_c=False)
     builder.run()
 
