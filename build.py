83e182dce5b4ba0532c398f5b6cdb76290ae5e64
Olivier Sechet <osechet@gmail.com>
2019-11-06 10:22:37 +0100

    Change build_policy

--- build.py
+++ build.py
@@ -4,7 +4,7 @@ from conan.packager import ConanMultiPackager
 def main():
     """ Main function """
 
-    builder = ConanMultiPackager(build_policy="missing")
+    builder = ConanMultiPackager(build_policy="outdated")
     builder.add_common_builds(pure_c=False)
     builder.run()
 
